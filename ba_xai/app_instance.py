8200bb8bdf4d86c5ac062199f56285ad32999d27
tc <tcinhoff@posteo.de>
2024-01-28 22:31:36 +0100

    kleines Update. Hatte probleme mit dem starten, weil sich der path irgendwie geändert hat. Sollte man im Blick behalten

--- ba_xai/app_instance.py
+++ ba_xai/app_instance.py
@@ -1,19 +1,22 @@
 from dash import Dash
 import pickle
 import dash_bootstrap_components as dbc
+import os
 
 PATHS = {
-    "model_path": "ba_xai/temp/model.pkl",
-    "train_data_path": "ba_xai/temp/train_data.csv",
-    "processed_train_data_path": "ba_xai/temp/processed_train_data.csv",
-    "test_data_path": "ba_xai/temp/test_data.csv",
-    "processed_test_data_path": "ba_xai/temp/processed_test_data.csv",
-    "prediction_path": "ba_xai/temp/prediction.csv",
-    "path_to_scaler": "ba_xai/temp/scaler.pkl",
-    "config_path": "ba_xai/temp/config.json",
+    "model_path": "temp/model.pkl",
+    "train_data_path": "temp/train_data.csv",
+    "processed_train_data_path": "temp/processed_train_data.csv",
+    "test_data_path": "temp/test_data.csv",
+    "processed_test_data_path": "temp/processed_test_data.csv",
+    "prediction_path": "temp/prediction.csv",
+    "path_to_scaler": "temp/scaler.pkl",
+    "config_path": "temp/config.json",
 }
 
 for path in PATHS.values():
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
     with open(path, "wb") as f:
         pickle.dump(None, f)
 
